ea5aa36fafe99f638611411fd40d5fd5e09faf9a
admin <admin>
2016-09-30 11:08:09 +0300

    add lists

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        Map<int, boolean> map = new HashMap<>();
+    }
+}
