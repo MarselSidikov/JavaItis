d3881ea2f1c7e000cf320f4f59576e7e741012a5
admin <admin>
2016-11-09 11:17:40 +0300

    add webconfig

---
+++ SpringController1/src/main/java/ru/itis/WebConfig.java
@@ -0,0 +1,32 @@
+package ru.itis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.itis")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    
+    @Bean
+    public ViewResolver configureViewResolver() {
+        InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
+        viewResolve.setPrefix("/WEB-INF/views/");
+        viewResolve.setSuffix(".jsp");
+
+        return viewResolve;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+}
