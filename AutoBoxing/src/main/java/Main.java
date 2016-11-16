e0d2111aa44a4fa300a58300419758bc72d7662f
admin <admin>
2016-11-16 11:25:19 +0300

    add boxing

---
+++ AutoBoxing/src/main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 16.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Integer n1 = 30;
+        Integer n2 = 30;
+
+        System.out.println(n1 == n2);
+
+        Integer n4 = 256;
+        Integer n5 = 256;
+
+        System.out.println(n4 == n5);
+    }
+}
